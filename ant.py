9fb56febe7c100f97a4a40b53c0265e743744a54
HuylenbroeckFlorent <huylenbroeck.florent@hotmail.fr>
2018-12-12 20:13:28 +0100

    colony growing

--- ant.py
+++ ant.py
@@ -1,28 +1,36 @@
+from __future__ import division
+
 from threading import Thread
 import numpy as np
 import numpy.random as np_random
 
 class ant(Thread):
-	def __init__(self, alpha, beta, permutation, pheromones):
+	def __init__(self, alpha, beta, Q, permutation, pheromones, swap_cost_function):
+		Thread.__init__(self)
 		self.alpha=alpha
 		self.beta=beta
+		self.Q=Q
 		self.permutation=permutation
 		self.pheromones=pheromones
+		self.swap_cost_function=swap_cost_function
 
 		self.size=len(permutation)
 		self.R=int(self.size/3)
+
+		self.local_search(self.permutation)
 		return
 
 	def run(self):
 		for i in range(self.R):
 			s=np_random.choice(range(self.size))
 			probs=self.compute_probability(self.permutation[s])
-			print(probs)
+			#print(probs)
 			r=np_random.choice(range(self.size),p=probs)
-			tmp=self.permutation[s]
-			self.permutation[s]=self.permutation[r]
-			self.permutation[r]=tmp
-			print(self.permutation)
+			self.swap_by_indexes(r,s)
+			#print(self.permutation)
+			self.local_search(self.permutation)
+			#print(self.permutation)
+			#self.drop_pheromones(self.permutation,self.Q)
 			i+=i
 		return
 
@@ -38,5 +46,18 @@ class ant(Thread):
 	def visibility(e1,e2):
 		return 1/D[e1][e2]
 
-	def drop_pheromones(s,Q):
-		return
+	def local_search(self,permutation):
+		for reps in range(2):
+			for i in np_random.permutation(range(self.size)):
+				for j in np_random.permutation(range(self.size)):
+					if j==i:
+						continue
+					if self.swap_cost_function(permutation,i,j)<0:
+						self.swap_by_indexes(i,j)
+			reps+=1
+		return permutation
+
+	def swap_by_indexes(self,i,j):
+		tmp=self.permutation[i]
+		self.permutation[i]=self.permutation[j]
+		self.permutation[j]=tmp
--- ant_colony.py
+++ ant_colony.py
@@ -28,8 +28,26 @@ def cost_function(s):
 			cost+=distances[i][j]*flows[s[i]][s[j]]
 	return cost
 
-def evaporate_pheromone(omega):
+def swap_cost_function(s,i,j):
+	cost=0
+	cost=(distances[i][i]-distances[j][j])*(flows[s[j]][s[j]]-flows[s[i]][s[i]])+(distances[i][j]-distances[j][i])*(flows[s[j]][s[i]]-flows[s[i]][s[j]])
+	for k in range(len(s)):
+		if k==j or k==i:
+			continue
+		cost+=(distances[k][i]-distances[k][j])*(flows[s[k]][s[j]]-flows[s[k]][s[i]])+(distances[i][k]-distances[j][k])*(flows[s[j]][s[k]]-flows[s[i]][s[k]])
+	return cost
+
+
+def evaporate_pheromone():
+	global pheromones
+	pheromones=(1-q)*pheromones
 	return
+
+def drop_pheromones(s,Q):
+		global pheromones
+		cost=cost_function(s)
+		for i in range(len(permutation)-1):
+			pheromones[permutation[i]][permutation[i+1]]+=Q/cost
 
 size=0
 distances=[]
@@ -38,6 +56,7 @@ N=10
 alpha=0.1
 beta=0.1
 Q=100
+q=0.1
 
 time.clock()
 if len(sys.argv)<3:
@@ -59,13 +78,30 @@ pheromones=np.ones((size,size))*(1/Q*current_min_cost)
 
 ants=[]
 for permutation in permutations:
-	ants.append(ant(alpha,beta,permutation,pheromones))
-
-ants[0].run()
-
+	ants.append(ant(alpha,beta,Q,permutation,pheromones,swap_cost_function))
 
 t_1=time.clock()
 
 #Make sure we're still within allocated time
 while time.clock()<max_time-t_1:
-	x=2
+
+	evaporate_pheromone()
+
+	for _ant in ants:
+		_ant.start()
+
+	for _ant in ants:
+		_ant.join()
+
+	print(sum([cost_function(_ant.permutation) for _ant in ants])/N)
+
+	for _ant in ants:
+		drop_pheromones(_ant.permutation,Q)
+
+	print(pheromones)
+
+	permutations=[_ant.permutation for _ant	in ants]
+
+	ants=[]
+	for permutation in permutations:
+		ants.append(ant(alpha,beta,Q,permutation,pheromones,swap_cost_function))
