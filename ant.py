faddb9adc3e73ffc293370585813ebf0b3646fe7
HuylenbroeckFlorent <huylenbroeck.florent@hotmail.fr>
2018-12-13 17:27:17 +0100

    almost done with these ants

--- ant.py
+++ ant.py
@@ -3,61 +3,54 @@ from __future__ import division
 from threading import Thread
 import numpy as np
 import numpy.random as np_random
+from copy import deepcopy
 
+from ant_util import swap_by_indexes
+
+#TODO document this shit
 class ant(Thread):
-	def __init__(self, alpha, beta, Q, permutation, pheromones, swap_cost_function):
+	def __init__(self, permutation, pheromones, cost_function, local_search, intensification, first):
 		Thread.__init__(self)
-		self.alpha=alpha
-		self.beta=beta
-		self.Q=Q
 		self.permutation=permutation
 		self.pheromones=pheromones
-		self.swap_cost_function=swap_cost_function
+		self.cost_function=cost_function
+		self.local_search=local_search
+		self.intensification=intensification
 
 		self.size=len(permutation)
 		self.R=int(self.size/3)
+		self.q=0.9
 
-		self.local_search(self.permutation)
+		if not first:
+			self.local_search(self.permutation)
 		return
 
 	def run(self):
+		prev_permutation=deepcopy(self.permutation)
+		prev_cost=self.cost_function(self.permutation)
 		for i in range(self.R):
-			s=np_random.choice(range(self.size))
-			probs=self.compute_probability(self.permutation[s])
-			#print(probs)
-			r=np_random.choice(range(self.size),p=probs)
-			self.swap_by_indexes(r,s)
-			#print(self.permutation)
+			self.pheromone_trail_swaps()
 			self.local_search(self.permutation)
-			#print(self.permutation)
-			#self.drop_pheromones(self.permutation,self.Q)
 			i+=i
+		if self.intensification and prev_cost<self.cost_function(self.permutation):
+			self.permutation=prev_permutation
+		return
+
+	def pheromone_trail_swaps(self):
+		s=np_random.choice(range(self.size))
+		probs=self.compute_probability(self.permutation[s])
+		if np_random.choice([True,False],p=[self.q,(1-self.q)]):
+			r=np.argmax(probs)
+		else:
+			r=np_random.choice(range(self.size),p=probs)
+		swap_by_indexes(self.permutation,s,r)
 		return
 
 	def compute_probability(self,s):
-		prob=np.zeros(self.size)
+		probs=np.zeros(self.size)
 		for element in self.permutation:
 			if element==s:
 				continue
-			prob[element]=self.pheromones[s][self.permutation[element]]+self.pheromones[element][self.permutation[s]]
-		prob/=sum(prob)
-		return prob
-
-	def visibility(e1,e2):
-		return 1/D[e1][e2]
-
-	def local_search(self,permutation):
-		for reps in range(2):
-			for i in np_random.permutation(range(self.size)):
-				for j in np_random.permutation(range(self.size)):
-					if j==i:
-						continue
-					if self.swap_cost_function(permutation,i,j)<0:
-						self.swap_by_indexes(i,j)
-			reps+=1
-		return permutation
-
-	def swap_by_indexes(self,i,j):
-		tmp=self.permutation[i]
-		self.permutation[i]=self.permutation[j]
-		self.permutation[j]=tmp
+			probs[element]=self.pheromones[s][self.permutation[element]]+self.pheromones[element][self.permutation[s]]
+		probs/=sum(probs)
+		return probs
